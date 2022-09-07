62d2a0bee3ef4fbd3d25522c96b4d80566c779a4
Adrian <acontrerasregueiro@gmail.com>
2022-09-07 12:14:03 +0200

    Ejercicio de importacion de modulos

---
+++ ejercicio1.py
@@ -0,0 +1,14 @@
+#En este ejercicio tendréis que crear un módulo
+# que contenga las operaciones básicas de una calculadora:
+# sumar, restar, multiplicar y dividir.
+
+#Este módulo lo importaréis a un archivo python
+# y llamaréis a las funciones creadas. 
+# Los resultados tenéis que mostrarlos por consola.
+
+import operaciones 
+
+print(operaciones.suma(4,4))
+print(operaciones.resta(4,4))
+print(operaciones.multiplizar(4,4))
+print(operaciones.dividir(4,4))
